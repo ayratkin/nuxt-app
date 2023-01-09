ec063cc61e7ce3ecbbf3ad11d2bfe11c9da51250
Ayrat <ayrat.klim1@yandex.ru>
2023-01-10 01:12:18 +0300

    init nuxt3 app, add first simple components

---
+++ nuxt-app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <AppNavigation/>
+    <NuxtPage/>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+
+}
+</script>
---
+++ nuxt-app/components/AppNavigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="nav">
+      <li>
+        <NuxtLink to="/" class="item"> Main page</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/page1"> Page1</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/page2"> Page2</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.nav {
+  display: flex;
+  list-style-type: none;
+
+}
+</style>
